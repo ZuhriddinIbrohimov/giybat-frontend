c2ab0b34090d1d9652777bda68af87660d8daf9f
ZuhriddinIbrohimov <151710292+ZuhriddinIbrohimov@users.noreply.github.com>
2025-05-18 19:15:54 +0500

    0.0.2

--- giybat-frontend-verstka-master/js/registration.js
+++ giybat-frontend-verstka-master/js/registration.js
@@ -1,48 +1,89 @@
+//document.getElementById("registrationForm")
+//    .addEventListener("submit", (event) => {
+//        event.preventDefault();
+//
+//        const name = document.getElementById("name").value;
+//        const phoneEmail = document.getElementById("phoneEmail").value;
+//        const password = document.getElementById("password").value;
+//        const confirmPassword = document.getElementById("confirmPassword").value;
+//        const errorTextTag = document.getElementById("errorText");
+//
+//        if (password !== confirmPassword) {
+//            errorTextTag.textContent = "Mazgimisiz nima balo?";
+//            errorTextTag.style.display = "block";
+//            document.getElementById("confirmPassword").style.borderColor = "red";
+//            document.getElementById("password").style.borderColor = "red";
+//            return;
+//        } else {
+//            errorTextTag.style.display = "none";
+//            document.getElementById("confirmPassword").style.borderColor = "#ddd";
+//            document.getElementById("password").style.borderColor = "#ddd";
+//        }
+//        //
+//
+//        const body = {
+//            "name": name,
+//            "username": phoneEmail,
+//            "password": password
+//        }
+//
+//        const lang = document.getElementById("current-lang").textContent;
+//    });
+//
+//
+//function checkEmailOrPhone(value) {
+//    // Regular expression for validating email
+//    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//    // Regular expression for validating phone numbers
+//    // Adjust based on your requirements (e.g., country-specific formats)
+//    const phoneRegex = /^998\d{9}$/; // 998 91 572 1213
+//
+//    if (emailRegex.test(value)) {
+//        return "Email";
+//    } else if (phoneRegex.test(value)) {
+//        return "Phone";
+//    } else {
+//        return "Invalid";
+//    }
+//}
 document.getElementById("registrationForm")
-    .addEventListener("submit", (event) => {
-        event.preventDefault();
+    .addEventListener("submit", (event)=> {
+            event.preventDefault();
+            const name = document.getElementById("name").value;
+            const phoneEmail = document.getElementById("phoneEmail").value;
+            const password = document.getElementById("password").value;
 
-        const name = document.getElementById("name").value;
-        const phoneEmail = document.getElementById("phoneEmail").value;
-        const password = document.getElementById("password").value;
-        const confirmPassword = document.getElementById("confirmPassword").value;
-        const errorTextTag = document.getElementById("errorText");
+            if( password !== confirmPassword ){
+//                console.log("teng emas");
+                errorTextTag.textContent="Mazgimisiz nima balo?"
+                errorTextTag.style.display="black";
+                document.getElementById("confirmPassword").style.borderColor("red");
+                document.getElementById("password").style.borderColor("red");
+                return;
+            }
+            else{
+                errorTextTag.style.display="none";
+                document.getElementById("confirmPassword").style.borderColor("#ddd");
+                document.getElementById("password").style.borderColor("#ddd");
+                console.log("teng")
+            }
+            console.log("mazgi keldi")
+            console.log(name+phoneEmail+password)
 
-        if (password !== confirmPassword) {
-            errorTextTag.textContent = "Mazgimisiz nima balo?";
-            errorTextTag.style.display = "block";
-            document.getElementById("confirmPassword").style.borderColor = "red";
-            document.getElementById("password").style.borderColor = "red";
-            return;
-        } else {
-            errorTextTag.style.display = "none";
-            document.getElementById("confirmPassword").style.borderColor = "#ddd";
-            document.getElementById("password").style.borderColor = "#ddd";
-        }
-        //
+      const body = {
+      "name":name,
+      "username":phoneEmail,
+      "password":password
+      }  // obyekt yaratildi
+      console.log(JSON.stringify(body));
 
-        const body = {
-            "name": name,
-            "username": phoneEmail,
-            "password": password
-        }
-
-        const lang = document.getElementById("current-lang").textContent;
-    });
+//      fetch("http://localhost:8080/auth/registration",{
+//            method:'POST',
+//            headers: {
+//                'Content-Type': 'application/json'
+//            },
+//            body: JSON.stringify()
+//      })
 
 
-function checkEmailOrPhone(value) {
-    // Regular expression for validating email
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // Regular expression for validating phone numbers
-    // Adjust based on your requirements (e.g., country-specific formats)
-    const phoneRegex = /^998\d{9}$/; // 998 91 572 1213
-
-    if (emailRegex.test(value)) {
-        return "Email";
-    } else if (phoneRegex.test(value)) {
-        return "Phone";
-    } else {
-        return "Invalid";
-    }
-}
+    })
